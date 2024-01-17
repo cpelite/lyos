4a951e6949656b0d03cdbb68f494144f5b26bc34
cpelite <cpelite@zlatina.tech>
2024-01-17 19:45:21 +0100

    https://os.phil-opp.com/vga-text-mode/ - stopped at "Volatile"

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std] 
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
